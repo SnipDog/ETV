524a356e16acd1ce313ddb3020ad06796d5b0d1d
Heisenbean <hedongbin1991@gmail.com>
2016-09-29 15:13:35 +0800

    init code

---
+++ ETV/Moudel/Main/MainTabBarController.swift
@@ -0,0 +1,47 @@
+//
+//  MainTabBarController.swift
+//  ETV
+//
+//  Created by Heisenbean on 16/9/29.
+//  Copyright © 2016年 Heisenbean. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setVc()
+    }
+    
+    
+    func setVc() {
+        addChildViewController("Home", "首页", UIImage(named:"menu_homepage_24x24_"), UIImage(named:"menu_homepage_sel_24x24_"))
+        addChildViewController("Home", "游戏", UIImage(named:"menu_youxi_24x24_"), UIImage(named:"menu_youxi_sel_24x24_"))
+        addChildViewController("Home", "娱乐", UIImage(named:"menu_yule_24x24_"),UIImage(named: "menu_yule_sel_24x24_"))
+        addChildViewController("Home", "女神", UIImage(named:"menu_goddess_normal_20x21_"), UIImage(named:"menu_goddess_20x21_"))
+        addChildViewController("Home", "我的", UIImage(named:"menu_mine_24x24_"), UIImage(named:"menu_mine_sel_24x24_"))
+    }
+    
+    fileprivate func addChildViewController(_ sbName: String, _ title: String, _ image: UIImage?, _ selectImage: UIImage?) {
+        let vc = UIStoryboard.initialViewController(sbName)
+        let nav = MainNavigationController(rootViewController: vc)
+        vc.title = title
+        nav.title = title
+        nav.tabBarItem.image = image!.withRenderingMode(.alwaysOriginal)
+        nav.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName:UIColor.init(colorLiteralRed: 36/255.0, green: 205/255.0, blue: 137/255.0, alpha: 1.0)], for: .selected)
+        nav.tabBarItem.selectedImage = selectImage!.withRenderingMode(.alwaysOriginal)
+        addChildViewController(nav)
+
+        
+    }
+
+
+// MARK: Others
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+ 
+}
---
+++ ETV/Others/Category/UIExtension.swift
@@ -0,0 +1,137 @@
+//
+//  UIExtension.swift
+//  ETV
+//
+//  Created by Heisenbean on 16/9/29.
+//  Copyright © 2016年 Heisenbean. All rights reserved.
+//
+
+import UIKit
+extension UIView {
+    
+    var x : CGFloat {
+        get {
+            return self.frame.origin.x
+        }
+        set(newX) {
+            self.frame.origin.x = newX
+        }
+    }
+    
+    var y : CGFloat {
+        get {
+            return self.frame.origin.y
+        }
+        set(newY) {
+            self.frame.origin.y = newY
+        }
+    }
+    
+    var width : CGFloat {
+        get {
+            return self.frame.size.width
+        }
+        set(newWidth) {
+            self.frame.size.width = newWidth
+        }
+    }
+    
+    var height : CGFloat {
+        get {
+            return self.frame.size.height
+        }
+        set(newHeight) {
+            self.frame.size.height = newHeight
+        }
+    }
+}
+
+extension CGRect {
+    var x : CGFloat {
+        get {
+            return self.origin.x
+        }
+        set(newX) {
+            self.origin.x = newX
+        }
+    }
+    
+    var y : CGFloat {
+        get {
+            return self.origin.y
+        }
+        set(newY) {
+            self.origin.y = newY
+        }
+    }
+    
+    var width : CGFloat {
+        get {
+            return self.size.width
+        }
+        set(newWidth) {
+            self.size.width = newWidth
+        }
+    }
+    
+    var height : CGFloat {
+        get {
+            return self.size.height
+        }
+        set(newHeight) {
+            self.size.height = newHeight
+        }
+    }
+    
+    
+}
+
+extension UIColor {
+    
+    
+    class func colorFromRGB(rgbValue: Int, alpha: CGFloat) -> UIColor {
+        
+        return UIColor(red: ((CGFloat)((rgbValue & 0xFF0000) >> 16))/255.0, green: ((CGFloat)((rgbValue & 0xFF00) >> 8))/255.0, blue: ((CGFloat)(rgbValue & 0xFF))/255.0, alpha: alpha)
+    }
+    
+    class func randomColor() -> UIColor {
+        
+        return UIColor(red: CGFloat(arc4random_uniform(256)) / 255, green: CGFloat(arc4random_uniform(256)) / 255, blue: CGFloat(arc4random_uniform(256)) / 255, alpha: 1.0)
+    }
+}
+
+
+extension UIStoryboard {
+    
+    class func initialViewController(_ name: String) -> UIViewController {
+        let sb = UIStoryboard(name: name, bundle: nil)
+        return sb.instantiateInitialViewController()!
+    }
+}
+
+extension UIScreen {
+    
+    class func isInch3_5() -> Bool {
+        
+        
+        return UIScreen.main.bounds.height == 480 ? true : false
+    }
+    
+    class func isInch4_0() -> Bool {
+        
+        
+        return UIScreen.main.bounds.height == 568 ? true : false
+    }
+    
+    class func isInch4_7() -> Bool {
+        
+        
+        return UIScreen.main.bounds.height == 667 ? true : false
+    }
+    
+    class func isInch5_5() -> Bool {
+        
+        
+        return UIScreen.main.bounds.height == 736 ? true : false
+    }
+}
