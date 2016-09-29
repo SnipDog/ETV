5ffa30ce89ad799785e0358b3ea2fa22792f9a1e
Heisenbean <hedongbin1991@gmail.com>
2016-09-29 13:30:52 +0800

    Initial Commit

---
+++ ETV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ETV
+//
+//  Created by Heisenbean on 16/9/29.
+//  Copyright © 2016年 Heisenbean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
